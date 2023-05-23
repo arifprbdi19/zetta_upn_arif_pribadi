45da9391e1acac1ceb800735ee23f914197a3f12
arifprbdi19 <123190019@student.upnyk.ac.id>
2023-05-23 22:55:35 +0700

    javascript day 4

--- main.js
+++ main.js
@@ -1,94 +1,97 @@
-const prompt = require("prompt-sync")({ sigint: true });
+const express = require("express");
+// const bodyParser = require("body-parser");
+const cors = require("cors");
 
-function countDiscount(price, piece, percent) {
-  percent /= 100; // division assignment
-  price -= price * percent; // subtraction assignment
-  return price * piece;
-}
+const app = express();
 
-function countTax(price, tax) {
-  return (price *= 1 + tax / 100); // multiplication assignment
-}
+app.use(express.json());
+// app.use(bodyParser.json());
+app.use(cors());
 
-function bookPurchasing(
-  title,
-  writer,
-  edition,
-  isbn,
-  price,
-  discount,
-  publisher,
-  stock,
-  amountOfPurchased,
-  termOfCredit
-) {
-  let inputUser;
+app.post("/bookPurchasing", isAuth, (req, res) => {
+  let bookDetails = req.body.bookDetails;
+  let termOfCredit = req.body.termOfCredit;
+  let purchasedAmount = req.body.purchasedAmount;
+
   const tax = 5;
-  let tempPrice = 0;
+  let originPrice = 0;
+  let discountPrice = 0;
+  let taxPrice = 0;
 
-  console.clear();
-  console.log(`===============================================`);
-  console.log(`====================PEMBELIAN==================`);
-  console.log(`===============================================`);
-  console.log(`Judul\t\t\t: ${title}`);
-  console.log(`Penulis\t\t\t: ${writer}`);
-  console.log(`Edisi\t\t\t: ${edition}`);
-  console.log(`Harga\t\t\t: ${price}`);
-  console.log(`Stok\t\t\t: ${stock}`);
-  console.log(`ISBN\t\t\t: ${isbn}`);
-  console.log(`Penerbit\t\t: ${publisher}`);
-  console.log(`Diskon\t\t\t: ${discount}% (-${(price * discount) / 100})`);
-  console.log(`Harga Setelah Diskon\t: ${countDiscount(price, 1, discount)}`);
-  console.log(`===============================================\n`);
+  // check data
+  if (!bookDetails.title) {
+    return res.status(400).json({
+      error: `Title is null`,
+    });
+  }
+  if (!bookDetails.writer) {
+    return res.status(400).json({
+      error: `Writer is null`,
+    });
+  }
+  if (!bookDetails.price) {
+    return res.status(400).json({
+      error: `Price is null`,
+    });
+  }
+  if (!bookDetails.discount) {
+    return res.status(400).json({
+      error: `Discount is null`,
+    });
+  }
+  if (!bookDetails.publisher) {
+    return res.status(400).json({
+      error: `Publisher is null`,
+    });
+  }
+  if (!bookDetails.stock) {
+    return res.status(400).json({
+      error: `Stock is null`,
+    });
+  }
+  if (!termOfCredit) {
+    return res.status(400).json({
+      error: `Term of Credit is null`,
+    });
+  }
+  if (!purchasedAmount) {
+    return res.status(400).json({
+      error: `Purchased Amount is null`,
+    });
+  }
 
-  for (let i = 0; i < 100; i++) {
-    inputUser = parseInt(prompt(`Jumlah Stok yang Akan Dibeli : `));
-    if (inputUser > stock) {
-      console.log(`Stok tidak cukup`);
-      continue;
-    } else {
-      stock -= inputUser; // subtraction assignment
-      amountOfPurchased += inputUser; // addition assignment
-      tempPrice += countDiscount(price, inputUser, discount); // addition assignment
-    }
-
-    // show summary
-    console.log(`Jumlah Sudah Dibeli\t: ${amountOfPurchased}`);
-    console.log(`Total Harga\t\t: ${tempPrice}\n`);
-    console.log(`PPN\t\t\t: ${tax}% (+${(tempPrice * tax) / 100})`);
-    console.log(`Total Harga + PPN\t: Rp. ${countTax(tempPrice, tax)},00-\n`);
-    console.log(`*Stok Sekarang\t: ${stock}\n`);
-    console.log(`===============================================`);
-
-    if (stock <= 0) {
-      console.log(`Maaf Tidak Dapat Menambah Pembelian\nStok Tidak Tersedia`);
-      break;
-    } else {
-      inputUser = prompt(`Beli lagi? (y/n) `);
-
-      // check input user if match/not
-      if (inputUser == "n" || inputUser == "N") {
-        break;
-      } else if (inputUser == "y" || inputUser == "Y") {
-        continue;
-      } else {
-        console.log(`Input salah`);
-        break;
-      }
-    }
+  if (purchasedAmount > bookDetails.stock) {
+    return res.status(400).json({
+      error: `Stok Tidak Cukup.`,
+    });
+  } else {
+    originPrice += bookDetails.price * purchasedAmount;
+    discountPrice += countDiscount(
+      bookDetails.price,
+      purchasedAmount,
+      bookDetails.discount
+    ); // addition assignment
+    taxPrice += countTax(discountPrice, tax);
   }
 
   // term of credit
-  let arrCredit = [];
   let arrMonth = [];
+  let arrCredit = [];
   for (var i = 0; i < termOfCredit; i++) {
     arrMonth.push(`Bulan ke-${i + 1}`);
-    arrCredit.push(countTax(tempPrice, tax) / termOfCredit);
+    arrCredit.push(taxPrice / termOfCredit);
   }
 
   let objCredit = [];
+  let remainingBalance = taxPrice;
   for (var i = 0; i < termOfCredit; i++) {
-    objCredit.push({ month: arrMonth[i], credit: arrCredit[i] });
+  remainingBalance -= arrCredit[i]
+
+    objCredit.push({
+      month: arrMonth[i],
+      credit: arrCredit[i],
+      remainingBalance : remainingBalance
+    });
   }
 
   const sumCredit = arrCredit.reduce(
@@ -99,27 +102,44 @@ function bookPurchasing(
   for (var i = 0; i < termOfCredit; i++) {
     console.log(`${objCredit[i].month}\t: ${objCredit[i].credit}`);
   }
-  console.log(`Total\t: ${sumCredit}`);
+  console.log(`Total\t\t: ${sumCredit}`);
 
+  return res.json({
+    bookDetails: {
+      title: bookDetails.title,
+      writer: bookDetails.writer,
+      publisher: bookDetails.publisher,
+      price: bookDetails.price,
+      discountPercent: bookDetails.discount,
+      remainStock: bookDetails.stock - purchasedAmount,
+    },
+    purchasedAmount: purchasedAmount,
+    totalOriginPrice: originPrice,
+    totalDiscountPrice: discountPrice,
+    taxPercent: tax,
+    totalPrice: taxPrice,
+    creditPayment: objCredit,
+  });
+});
+
+app.listen(3000, () => console.log("Api Server is running..."));
+
+function isAuth(req, res, next) {
+  const auth = req.headers.authorization;
+  if (auth === "your_password") {
+    next();
+  } else {
+    res.status(401);
+    res.send("Access forbidden");
+  }
 }
 
-function main() {
-  console.clear();
-  const stock = prompt(`Jumlah Stok Awal = `);
-  const termOfCredit = prompt(`Cicilan (bulan) = `);
-
-  bookPurchasing(
-    "Lost In The Jungle",
-    "Yossi Ghinsberg",
-    "1st",
-    "978-602-00-1175",
-    60000,
-    "10",
-    "Elex Media Komputindo",
-    stock,
-    null,
-    termOfCredit
-  );
+function countDiscount(price, piece, percent) {
+  percent /= 100; // division assignment
+  price -= price * percent; // subtraction assignment
+  return price * piece;
 }
 
-main();
+function countTax(price, tax) {
+  return (price *= 1 + tax / 100); // multiplication assignment
+}
